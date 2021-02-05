4eb5aa99b8ab1f1675bc25172a61a0ab0e8808fc
Aman Kanatbek Uulu <bishkek9408@gmail.com>
2021-02-05 12:46:54 +0600

    #1 initional commit add method byBreed

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            var cats = Cat.makeCats(10);
+            Printer.print(cats);
+
+            cats.sort(Cat::sortByBread);
+            Printer.print(cats);
+
+
+
+            // А сюда добавьте код, который будет сортировать коллекцию котов
+            // используйте лямбда-выражения и ссылки на методы
+            //cats.sort(?);
+            //Printer.print(cats);
+
+    }
+}
---
+++ src/com/company/Printer.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.List;
+
+public class Printer {
+    private static final void printHeader() {
+        System.out.println(String.format("%1$10.10s | %1$3.3s | %1$-10.10s | %1$-10.10s |", "--------------"));
+        System.out.println(String.format("%10.10s | %3.3s | %-10.10s | %-10.10s |", "Name", "Age", "Breed", "Color"));
+        System.out.println(String.format("%1$10.10s | %1$3.3s | %1$-10.10s | %1$-10.10s |", "--------------"));
+    }
+
+    private static final void printCat(Cat cat) {
+        System.out.println(String.format("%10.10s | %3.3s | %-10.10s | %-10.10s |", cat.getName(), cat.getAge(), cat.getBreed(), cat.getColor()));
+    }
+
+    public static final void print(List<Cat> cats) {
+        printHeader();
+        cats.forEach(Printer::printCat);
+        System.out.println();
+    }
+}
